40b8a8c5b97ef5c980468e538e998916f61bc91f
Historia Montague <historia.montague@outlook.com>
2023-05-23 12:40:23 -0600

    Finished Lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+  def __init__(self, value =''):
+    self.value = value
+
+  def set_value(self):
+    return self._value
+  
+  def get_value(self, value):
+    try:
+      if(type(value) == str):
+        self._value = value
+      else:
+        raise TypeError
+    except TypeError:
+      print("The value must be a string.")
+  
+  def is_sentence(self):
+    return self.value.endswith(".")
+  
+  def is_question(self):
+    return self.value.endswith("?")
+  
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def count_sentences(self):
+    return len(re.findall("[!?.]+(?=$|\s)", self.value))
+  
+  value = property(set_value, get_value)
